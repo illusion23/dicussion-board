ea580352c755c47b07243e5fd74019fbe278a0be
illusion23 <belghoul.achref23@gmail.com>
2020-12-05 23:27:14 +0100

    boards

---
+++ first_project/boards/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Board(models.Model):
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=50,unique=True)
+    description = models.TextField(max_length=150)
+
+class Topic(models.Model):
+    subject = models.CharField(max_length=250)
+    board   = models.ForeignKey(Board,related_name='topics',on_delete=models.CASCADE) #ForeignKey = Many2one
+    create_by = models.ForeignKey(User,related_name='topics',on_delete=models.CASCADE)
+    create_on = models.DateTimeField(auto_now_add=True)
+
+class Post(models.Model):
+    message = models.TextField(max_length=4000)
+    topic   = models.ForeignKey(Topic,related_name='posts',on_delete=models.CASCADE)
+    create_by = models.ForeignKey(User,related_name='posts',on_delete=models.CASCADE)
+    create_on = models.DateTimeField(auto_now_add=True)
+
---
+++ first_project/boards/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from .models import Board
+# Create your views here.
+
+def home(request):
+
+    boards = Board.objects.all()
+    return render(request,'home.html',{'boards':boards})
+
+
+
+def board_topics(request,board_id):
+    try:
+        board = Board.objects.get(pk=board_id)
+    except Board.DoesNotExist:
+        return render(request,'error404.html')
+
+    return render(request,'topics.html',{'board':board})
+
+def new_topic(request,board_id):
+    pass
